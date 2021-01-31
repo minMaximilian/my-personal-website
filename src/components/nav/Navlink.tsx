f523613109ccc687776d44f10cc4f24d09de1f1f
Max <sebastian.8295.8295@gmail.com>
2021-01-31 01:33:04 +0000

    added pop up menu once you press the burger

--- src/components/nav/Navlink.tsx
+++ src/components/nav/Navlink.tsx
@@ -6,15 +6,24 @@ interface Props {
 }
 
 const WrapperDiv = styled.div<Props>`
-  ${Props => Props.visible ? `
-    display: none;
-  ` : `
-    display: flex;
-  `}
-  display: flex;
-  flex-direction: row-reverse;
-  grid-column-start: 3;
+  @media only screen and (max-width: 1023px) {
+      ${Props => Props.visible ? `
+      display: none;
+    ` : `
+      display: flex;
+      position: absolute;
+      background-color: #fff;
+      border-right: 1px ${Props.theme.primaryColour} solid;
+      border-top: 1px ${Props.theme.primaryColour} solid;
+      top: 4rem;
+      height: calc(100vh - 4rem);
+      width: 20rem;
+    `}
+  }
+
   @media only screen and (min-width: 1024px) {
+    display: flex;
+    flex-direction: row-reverse;
     grid-column-start: 4;
   }
 `
